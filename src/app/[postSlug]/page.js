3b05af7bae503c79342f44886a1574987cad535b
michael kostal <kostal@gmail.com>
2024-03-01 13:20:34 -0600

    add metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -7,6 +7,16 @@ import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+// Generate metadata
+export async function generateMetadata({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+  const { frontmatter: { title, abstract: description } } = post;
+  return {
+    title,
+    description
+  };
+}
+
 async function BlogPost({params}) {
   const post = await loadBlogPost(params.postSlug);
   const { frontmatter: { title, publishedOn }, content } = post;
