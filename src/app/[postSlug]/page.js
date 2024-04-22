2a358aaf2df5a8de504eb3b5a99b4b99f2b47d10
michael kostal <Michael.Kostal@united.com>
2024-04-22 09:15:02 -0500

    Update page.js

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,55 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import BlogHero from '@/components/BlogHero';
+import BlogHero from "@/components/BlogHero";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
 
-import { loadBlogPost,getBlogPostList } from '@/helpers/file-helpers';
+import { loadBlogPost, getBlogPostList } from "@/helpers/file-helpers";
 
-import COMPONENT_MAP from '@/helpers/mdx-helpers';
+import COMPONENT_MAP from "@/helpers/mdx-helpers";
 
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-import { BLOG_TITLE } from '@/constants';
+import { BLOG_TITLE } from "@/constants";
 
+async function BlogPost({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+  if (!post) {
+    return "Post not found."; // TODO: 404 component
+  }
+  const {
+    frontmatter: { title, publishedOn },
+    content,
+  } = post;
 
+  return (
+    <article className={styles.wrapper}>
+      <BlogHero title={title} publishedOn={publishedOn} />
+      <div className={styles.page}>
+        <MDXRemote source={content} components={COMPONENT_MAP} />
+      </div>
+    </article>
+  );
+}
 
+// Generate metadata
+export async function generateMetadata({ params }) {
+  const post = await loadBlogPost(params.postSlug);
+  const {
+    frontmatter: { title, abstract: description },
+  } = post;
+  return {
+    title: `${title} • ${BLOG_TITLE}`,
+    description,
+  };
+}
 export async function generateStaticParams() {
   const blogPosts = await getBlogPostList();
 
   return blogPosts.map(({ slug }) => ({
     postSlug: slug,
   }));
-}
-
-// Generate metadata
-export async function generateMetadata({ params }) {
-  const post = await loadBlogPost(params.postSlug);
-  const { frontmatter: { title, abstract: description } } = post;
-  return {
-    title: `${title} • ${BLOG_TITLE}`,
-    description
-  };
-}
-
-async function BlogPost({params}) {
-  const post = await loadBlogPost(params.postSlug);
-  const { frontmatter: { title, publishedOn }, content } = post;
-  
-  return (
-    <article className={styles.wrapper}>
-      <BlogHero
-        title={title}
-        publishedOn={publishedOn}
-      />
-      <div className={styles.page}>
-          <MDXRemote
-            source={content}
-            components={COMPONENT_MAP}
-          />
-      </div>
-    </article>
-  );
 }
 
 export default BlogPost;
